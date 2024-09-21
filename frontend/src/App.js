755cb5a89bd69efc82035b900e4445d1db62c8fb
ces20 <ces234@case.edu>
2024-09-21 10:53:28 -0400

    Nav Bar (frontend)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeSuggestionPage from './Pages/Recipe Suggestion Page/RecipeSuggestionPage';
-
+import NavBar from './Universal Components/Nav Bar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar /> 
         <Routes>
           <Route path="/" element={<RecipeSuggestionPage />} /> {/* Home page */}
         </Routes>
