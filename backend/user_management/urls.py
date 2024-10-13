c987f19c5b1e0bfddacacb5ec1c1f5f9fe042bfb
ces20 <ces234@case.edu>
2024-10-13 15:26:22 -0400

    sign up functionality

--- backend/user_management/urls.py
+++ backend/user_management/urls.py
@@ -4,6 +4,7 @@ from .views import UserListView, HouseholdListView
 from .views import LoginView
 from .views import recently_added_recipes
 from .views import add_recently_added_recipe
+from .views import signup
 
 
 
@@ -14,5 +15,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('recently-added-recipes/', recently_added_recipes, name='recently_added_recipes'),
     path('add-recently-added-recipe/', add_recently_added_recipe, name='add_recently_added_recipe'),
-
+    path('signup/', signup, name = "signup"),
 ]
