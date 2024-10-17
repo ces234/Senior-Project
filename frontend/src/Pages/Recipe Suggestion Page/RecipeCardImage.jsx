fe2779fb6bf2686f25505578a71cd2db94a3a808
jan116 <jnorek@gmail.com>
2024-10-17 16:09:03 -0400

    Added Photos
    added photos and  + some parts of hierarchy

--- frontend/src/Pages/Recipe Suggestion Page/RecipeCardImage.jsx
+++ frontend/src/Pages/Recipe Suggestion Page/RecipeCardImage.jsx
@@ -1,9 +1,45 @@
 import airFryer from "../../photos/airFryer.jpg";
+import brownies from "../../photos/brownies.webp";
+import buffaloWings from "../../photos/buffaloWings.jpeg";
+import Canning from "../../photos/Canning.webp";
+import cheeseBalls from "../../photos/cheeseBalls.webp";
+import cheesecake from "../../photos/cheesecake.webp";
+import dumplings from "../../photos/dumplings.webp";
+import falafel from "../../photos/falafel.webp";
+import fondue from "../../photos/falafel.webp";
+import porkRibs from "../../photos/porkRibs.jpeg";
 
 const RecipeCardImage = ({ categories }) => {
     const getImage = () => {
-        if (categories.includes("Beef Recipes")) {
+        if (categories.includes("Air Fryer Recipes")) {
             return airFryer; // Return the image path directly
+        }
+        if (categories.includes("Canning and Preserving")) {
+            return Canning; // Return the image path directly
+        }
+        if (categories.includes("Cheese Fondue")) {
+            return fondue; // Return the image path directly
+        }
+        if (categories.includes("Cheesecake")) {
+            return cheesecake; // Return the image path directly
+        }
+        if (categories.includes("Cheese Balls")) {
+            return cheeseBalls; // Return the image path directly
+        }
+        if (categories.includes("Dumplings")) {
+            return dumplings; // Return the image path directly
+        }
+        if (categories.includes("Buffalo Chicken Wings")) {
+            return buffaloWings; // Return the image path directly
+        }
+        if (categories.includes("Brownies")) {
+            return brownies; // Return the image path directly
+        }
+        if (categories.includes("Falafel")) {
+            return falafel; // Return the image path directly
+        }
+        if (categories.includes("Pork Ribs")) {
+            return porkRibs; // Return the image path directly
         }
         return ""; // Or return a default image path or null
     };
