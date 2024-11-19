1fdab6b549bd887ca9c7a0b931f6c7a93e4b67e9
ces20 <ces234@case.edu>
2024-11-18 22:44:41 -0500

    rating frontend

--- frontend/src/Pages/Recipe Page/RecipePage.jsx
+++ frontend/src/Pages/Recipe Page/RecipePage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useAuth } from "../../AuthContext";
-import './RecipePage.css';
+import "./RecipePage.css";
 
 const RecipePage = () => {
   const { id } = useParams();
@@ -10,7 +10,6 @@ const RecipePage = () => {
   const [pantryIngredients, setPantryIngredients] = useState([]);
   const [groceryIngredients, setGroceryIngredients] = useState([]);
   const { user, logout, isMember } = useAuth(); // Use isMember from context
-  const [rating, setRating] = useState(null);
   const [submittedRating, setSubmittedRating] = useState(false);
 
   console.log(isMember()); // This will show true or false
@@ -31,6 +30,10 @@ const RecipePage = () => {
         );
         const data = await response.json();
         setRecipe(data);
+
+        if (data.ratings.some((rating) => rating.username === user.username)) {
+          setSubmittedRating(true);
+        }
       } catch (error) {
         console.error("Error fetching recipe:", error);
       }
@@ -79,39 +82,20 @@ const RecipePage = () => {
       fetchPantryIngredients();
       fetchGroceryList();
     }
-  }, [id, token, submittedRating]);
+  }, [id, token, submittedRating, user.username]);
 
-  const handleRatingSubmit = async () => {
+  const handleRequestRecipe = async () => {
     try {
       const response = await fetch(
-        `http://localhost:8000/recipes/recipe/${id}/rate/`,
+        `http://localhost:8000/recipes/request/${id}/`,
         {
           method: "POST",
           headers: {
             Authorization: `Token ${token}`,
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ rating }),
         }
       );
-
-      if (!response.ok) throw new Error("failed to submit rating");
-
-      setSubmittedRating(true);
-    } catch (error) {
-      console.error("Error submitting rating:", error);
-    }
-  };
-
-  const handleRequestRecipe = async () => {
-    try {
-      const response = await fetch(`http://localhost:8000/recipes/request/${id}/`, {
-        method: "POST",
-        headers: {
-          Authorization: `Token ${token}`,
-          "Content-Type": "application/json",
-        },
-      });
 
       if (!response.ok) throw new Error("Failed to request recipe");
 
@@ -126,128 +110,180 @@ const RecipePage = () => {
   }
 
   const RecipeDetails = ({ recipe }) => {
-  return (
-    <div className="recipe-details">
-      <div className="detail">
-        <span>Prep Time:</span> {recipe.prep_time} min
+    return (
+      <div className="recipe-details">
+        <div className="detail">
+          <span>Prep Time:</span> {recipe.prep_time} min
+        </div>
+        <div className="detail">
+          <span>Cook Time:</span> {recipe.cook_time} min
+        </div>
+        <div className="detail">
+          <span>Servings:</span> {recipe.servings}
+        </div>
+        <div className="detail">
+          {/* <span>Avg. Rating:</span> {recipe.ratings} 
+        //TODO: compute avg ratings */}
+        </div>
       </div>
-      <div className="detail">
-        <span>Cook Time:</span> {recipe.cook_time} min
+    );
+  };
+
+  const Instructions = () => {
+    // Split instructions into sentences, filtering out any empty strings
+    const instructionsList = recipe.instructions
+      .split(".")
+      .map((sentence) => sentence.trim()) // Remove extra whitespace
+      .filter((sentence) => sentence); // Remove any empty strings
+
+    return (
+      <div className="instructions">
+        <ol>
+          {instructionsList.map((sentence, index) => (
+            <li key={index}>{sentence}.</li>
+          ))}
+        </ol>
       </div>
-      <div className="detail">
-        <span>Servings:</span> {recipe.servings}
-      </div>
-      <div className="detail">
-        <span>Avg. Rating:</span> {recipe.ratings} 
-        {/* //TODO: compute avg ratings */}
-      </div>
-    </div>
-  );
-};
-
-const Instructions = () => {
-
-  // Split instructions into sentences, filtering out any empty strings
-  const instructionsList = recipe.instructions
-    .split('.')
-    .map((sentence) => sentence.trim()) // Remove extra whitespace
-    .filter((sentence) => sentence); // Remove any empty strings
-
-  return (
-    <div className="instructions">
-      <ol>
-        {instructionsList.map((sentence, index) => (
-          <li key={index}>{sentence}.</li>
-        ))}
-      </ol>
-    </div>
-  );
-};
-
+    );
+  };
 
   const Ingredients = () => {
     return (
-        <div className="ingredients">
-          <ul>
-            {recipe.ingredients.map((ingredient, index) => (
-              <li key={index}>
-                {ingredient.quantity}{' '}
-                {ingredient.unit}{' '}
-                {ingredient.name}
-                {pantryIngredients.some(
-                  (pantryItem) => pantryItem.ingredient_name === ingredient.name
-                ) && <span> <InPantryTag/> </span>}
-                {groceryIngredients &&
-                  groceryIngredients.some(
-                    (groceryItem) => groceryItem.name === ingredient.name
-                  ) && <span> (in grocery list)</span>}
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="ingredients">
+        <ul>
+          {recipe.ingredients.map((ingredient, index) => (
+            <li key={index}>
+              {ingredient.quantity} {ingredient.unit} {ingredient.name}
+              {pantryIngredients.some(
+                (pantryItem) => pantryItem.ingredient_name === ingredient.name
+              ) && (
+                <span>
+                  {" "}
+                  <InPantryTag />{" "}
+                </span>
+              )}
+              {groceryIngredients &&
+                groceryIngredients.some(
+                  (groceryItem) => groceryItem.name === ingredient.name
+                ) && <span> (in grocery list)</span>}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+  const renderStars = (rating) => {
+    return (
+      <div className="stars">
+        {[...Array(5)].map((_, index) => (
+          <span
+            key={index}
+            className={`star ${index < rating ? "filled" : ""}`}
+          >
+            ★
+          </span>
+        ))}
+      </div>
     );
   };
 
   const Ratings = () => {
+    const [hoveredStar, setHoveredStar] = useState(0);
+    const [rating, setRating] = useState(null);
+    const [selectedRating, setSelectedRating] = useState(0);
+
+    const handleMouseOver = (index) => setHoveredStar(index + 1);
+    const handleMouseOut = () => setHoveredStar(0);
+    const handleClick = (index) => {
+      setRating(index + 1);
+      setSelectedRating(index + 1);
+    };
+
+    const handleRatingSubmit = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8000/recipes/recipe/${id}/rate/`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Token ${token}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ rating }),
+          }
+        );
+
+        if (!response.ok) throw new Error("Failed to submit rating");
+        setSubmittedRating(true);
+      } catch (error) {
+        console.error("Error submitting rating:", error);
+      }
+    };
+
     return (
       <div className="ratings">
         <ul>
-          {recipe.ratings.map((rating, index) => (
+          {recipe.ratings.map((ratingObj, index) => (
             <li key={index}>
-              {rating.username}: {rating.rating}{" "}
-              {rating.rating === 1 ? "star" : "stars"}
+              <span className="username">{ratingObj.username}:</span>
+              <span className="stars">
+                {renderStars(ratingObj.rating)}
+              </span>{" "}
             </li>
           ))}
         </ul>
-        <div className="rating-section">
-        <h2>Rate this Recipe</h2>
-        {!submittedRating ? (
-          <>
-            <select
-              value={rating}
-              onChange={(e) => setRating(Number(e.target.value))}
-            >
-              <option value="">Select rating</option>
-              {[1, 2, 3, 4, 5].map((value) => (
-                <option key={value} value={value}>
-                  {value} {value === 1 ? "star" : "stars"}
-                </option>
+        {!submittedRating && (
+          <div className="rating-section">
+            <h2 className = "rateRecipeText">Rate this Recipe</h2>
+            <div className="stars">
+              {[...Array(5)].map((_, index) => (
+                <span
+                  key={index}
+                  className={`star ${
+                    hoveredStar > index || selectedRating > index
+                      ? "filled"
+                      : ""
+                  }`}
+                  onMouseOver={() => handleMouseOver(index)}
+                  onMouseOut={handleMouseOut}
+                  onClick={() => handleClick(index)}
+                >
+                  ★
+                </span>
               ))}
-            </select>
-            <button onClick={handleRatingSubmit}>Submit Rating</button>
-          </>
-        ) : (
-          <p>Thank you for rating!</p>
+            </div>
+            <button className="submitRatingButton" onClick={handleRatingSubmit}>
+              Submit Rating
+            </button>
+          </div>
         )}
-      </div>
       </div>
     );
   };
 
   const InPantryTag = () => {
-    return (
-      <span className="ingredient-tag">In Pantry</span>
-    )
+    return <span className="ingredient-tag">In Pantry</span>;
   };
-  
+
   return (
     <div className="recipe-container">
       <h1>{recipe.name} Recipe</h1>
 
       <div>
-        <RecipeDetails recipe={recipe}/>
+        <RecipeDetails recipe={recipe} />
       </div>
 
       <div className="details-container">
         <div className="instructions-container">
           <h2>Instructions</h2>
-          <Instructions/>
+          <Instructions />
         </div>
         <div className="ingredients-ratings-container">
           <h2>Ingredients</h2>
-          <Ingredients/>
+          <Ingredients />
           <h2>Family Ratings</h2>
-          <Ratings/>
+          <Ratings />
         </div>
       </div>
 
