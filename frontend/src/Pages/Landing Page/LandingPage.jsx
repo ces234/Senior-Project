3b5dd5545aa2bed920f269f06cfd015e39395f08
PCahill44 <ptc34@case.edu>
2024-11-30 15:17:23 -0500

    Landing page and navbar updates

--- frontend/src/Pages/Landing Page/LandingPage.jsx
+++ frontend/src/Pages/Landing Page/LandingPage.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Correct import for useNavigate
+import { useNavigate } from 'react-router-dom';
+import './LandingPage.css'; 
+import WholeLogo from './Whole_logo.png';
+
 
 const LandingPage = () => {
-    const navigate = useNavigate(); // Use useNavigate hook
+    const navigate = useNavigate();
 
     const handleLogin = () => {
-        navigate("/login"); // Use navigate function to redirect
-    }
+        navigate("/login");
+    };
 
     const handleSignup = () => {
-        navigate ("/signup");
-    }
+        navigate("/signup");
+    };
 
     return (
-        <div>
-            <button onClick={handleLogin}>Login</button>
-            <button onClick={handleSignup} >Sign up</button>
+        <div className="landingPageContainer">
+            <div className="screen">
+                <img src={WholeLogo} alt="Center Display" className="centerImage" />
+            </div>
+            <div className="buttonContainer">
+                <button className="landingButton" onClick={handleLogin}>Login</button>
+                <button className="landingButton" onClick={handleSignup}>Register</button>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default LandingPage;
